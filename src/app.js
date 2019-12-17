01fef0a259396ad36cdbfac0e7fdeb37daf74b15
SpencerAung <10285698+SpencerAung@users.noreply.github.com>
2019-12-17 14:58:31 +0900

    feat: parse currencies

--- src/app.js
+++ src/app.js
@@ -4,11 +4,26 @@ const { buildSchema } = require('graphql')
 const fetch = require('node-fetch')
 
 const schema = buildSchema(`
+  enum  CurrencyType {
+    FIAT
+    CRYPTO
+  }
+
   type Currency {
     """
     Currency acronym
     """
     acronym: String
+
+    """
+    Currency Id
+    """
+    currencyId: String
+
+    """
+    Currency type: FIAT or CRYPTO
+    """
+    type: CurrencyType
   }
 
   type Query {
@@ -22,8 +37,22 @@ const fetchQuery = async (url) => {
   return data.result
 }
 
-const fetchCurrencies = () => {
-  return fetchQuery('https://api-9f2d25efde52db4ab5bc.korbit.co.kr/v1/currencies')
+const parseCurrencies = (currencies) => {
+  if (!Array.isArray(currencies)) {
+    return []
+  }
+
+  /* eslint-disable-next-line */
+  return currencies.map(({ acronym, currency_id, currency_type }) => ({
+    acronym,
+    currencyId: currency_id,
+    type: (currency_type || '').toUpperCase() /* eslint-disable-line */
+  }))
+}
+
+const fetchCurrencies = async () => {
+  const currencies = await fetchQuery('https://api-9f2d25efde52db4ab5bc.korbit.co.kr/v1/currencies')
+  return parseCurrencies(currencies)
 }
 
 const resolvers = {
