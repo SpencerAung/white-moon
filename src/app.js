01cfc949698585516e84570fc7166cbe7e415dd9
SpencerAung <10285698+SpencerAung@users.noreply.github.com>
2019-12-18 12:19:35 +0900

    feat: move fetching currencies logic to api

--- src/app.js
+++ src/app.js
@@ -2,25 +2,7 @@ const express = require('express')
 const graphqlHTTP = require('express-graphql')
 
 const schema = require('./schema')
-const { fetchQuery } = require('./api')
-
-const parseCurrencies = (currencies) => {
-  if (!Array.isArray(currencies)) {
-    return []
-  }
-
-  /* eslint-disable-next-line */
-  return currencies.map(({ acronym, currency_id, currency_type }) => ({
-    acronym,
-    currencyId: currency_id,
-    type: (currency_type || '').toUpperCase() /* eslint-disable-line */
-  }))
-}
-
-const fetchCurrencies = async () => {
-  const currencies = await fetchQuery('https://api-9f2d25efde52db4ab5bc.korbit.co.kr/v1/currencies')
-  return parseCurrencies(currencies)
-}
+const { fetchCurrencies } = require('./api')
 
 const resolvers = {
   currencies: () => {
