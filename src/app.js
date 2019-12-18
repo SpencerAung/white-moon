e3a4b3028c45932eee2999cc3002428b288ab3a3
SpencerAung <10285698+SpencerAung@users.noreply.github.com>
2019-12-18 16:34:45 +0900

    feat: add logging

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
 const express = require('express')
 const graphqlHTTP = require('express-graphql')
+const bunyan = require('bunyan')
+const bodyParser = require('body-parser')
 
 const schema = require('./schema')
 const resolvers = require('./resolvers')
 
+const log = bunyan.createLogger({ name: 'white-moon' })
+
 const app = express()
 app.set('port', 3000)
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use((req, res, next) => {
+  log.info(req.body)
+  next()
+})
 app.use('/graphql', graphqlHTTP({
   schema,
   rootValue: resolvers,
